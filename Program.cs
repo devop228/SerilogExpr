e7be50e8d2d9e941c0f8114b7b437f6d25a6a73b
Yu Zhu <devop228@zhusmelb.com>
2019-12-20 16:56:10 +1100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SerilogExpr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
