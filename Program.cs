6c1a839fe39f671af6aea7205973d046509a7aac
Yu Zhu <devop228@zhusmelb.com>
2019-12-25 23:13:01 +1100

    Enable Ai for Serilog.
    Use LoggerConfigurate.CreateLogger to create logger.
    Configure Ai with appsettings.json file.
    InstrumentationKey is under
    
    Logging:Serilog:WriteTo:1:Args:InstrumentationKey
    
    1 is the WriteTo[1], which is the ApplicationInsights sink.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace SerilogExpr
 {
@@ -6,7 +9,21 @@ namespace SerilogExpr
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var configRoot = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddUserSecrets(typeof(Program).Assembly)
+                .Build();
+
+            var logSect = configRoot.GetSection("Logging");
+            var k = logSect.GetValue<string>("Serilog:WriteTo:1:Args:InstrumentationKey");
+            var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(logSect)
+                .CreateLogger();
+
+            logger.Information("Hellow {name}!", "Yu");
+            logger.Information("Hello {person}", new {FirstName="Yu", LastName="Zhu"});
+            Console.ReadKey();
         }
     }
 }
